031a1cb5f979d15e200d6c0b882f5c50047bf885
soadfreyle <soadfre@gmail.com>
2022-12-23 10:37:11 -0500

    error carga duplicada

--- src/main.js
+++ src/main.js
@@ -14,11 +14,12 @@ const api = axios.create({
 
 async function getTrendingMoviesPreview() {
     const { data } = await api('trending/movie/day');
-
     const movies = data.results;
-    movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
+    trendingMoviesPreviewList.innerHTML = "";
+    
+    movies.forEach(movie => {
+        //const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList')
 
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
@@ -35,10 +36,12 @@ async function getTrendingMoviesPreview() {
 
 async function getCategoriesPreview() {
     const { data } = await api('genre/movie/list');
-
     const categories = data.genres;
+
+    categoriesPreviewList.innerHTML = "";
+
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
+        //const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list')
 
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
